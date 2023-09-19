5f705d0d10887e9bd5dcc29e0cd2c65094bc9802
Pritchad25 <pricts29@gmail.com>
2023-09-19 03:30:12 -0700

    Addition of the class attribute places which represents a relationship with the class Place.

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """
@@ -20,6 +21,10 @@ class User(BaseModel, Base):
         containing a string (128 characters). Attribute can be null.
         last_name (str): attribute represents a column
         containing a string (128 characters).Attribute can be null.
+        places (relationship): this represents a relationship with the
+        class Place. If the User object is deleted, all linked Place objects
+        must be automatically deleted. The reference from a Place object
+        to its User should be named "user".
 
     Args:
         BaseModel (class): the BaseModel class.
@@ -32,3 +37,4 @@ class User(BaseModel, Base):
      password = Column(String(128), nullable=False)
      first_name = Column(String(128), nullable=True)
      last_name = Column(String(128), nullable=True)
+     places = relationship('Place', back_populates='user', cascade='all, delete-orphan')
