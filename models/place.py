830da9542f6f670390556e05108bac36bd0fef7e
shazaaly <shaza.aly@gmail.com>
2023-09-20 21:18:55 +0300

    storage type

--- models/place.py
+++ models/place.py
@@ -23,30 +23,37 @@ class Place(BaseModel, Base):
     The definition of the Place class for the places table in the database.
     """
     __tablename__ = 'places'
-
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-
-    amenities_ids = []
-
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+
+        amenities_ids = []
+
         amenities = relationship("Amenity", secondary=place_amenity,
                                  viewonly=False,
                                  back_populates="place_amenities")
 
     else:
-        """Getter attribute amenities that returns
-        the list of Amenity instances based on the
-        attribute amenity_ids that contains all
-        Amenity.id linked to the Place"""
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
 
         @property
         def amenities(self):
