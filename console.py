b228545914928f4c3d653a400d9e294e4d1234aa
shazaaly <shaza.aly@gmail.com>
2023-09-21 01:48:14 +0300

    fix

--- console.py
+++ console.py
@@ -140,7 +140,7 @@ class HBNBCommand(cmd.Cmd):
             attr_value = attr_value.replace('"', r'\"')
             attr_value = attr_value.replace('_', ' ')
 
-            if '.' in attr_value and attr_name != 'email':
+            if '.' in attr_value:
                 attr_value = float(attr_value)
             elif attr_value.isdigit():
                 attr_value = int(attr_value)
--- models/city.py
+++ models/city.py
@@ -6,14 +6,19 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import relationship
 from models.base_model import Base
+import os
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
 
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship('State', back_populates='cities')
-    places = relationship('Place', backref='cities',
-                          cascade='all, delete, delete-orphan')
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        state = relationship('State', back_populates='cities')
+        places = relationship('Place', backref='cities',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
+        state_id = ""
