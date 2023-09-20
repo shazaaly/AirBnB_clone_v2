70f2758355032a391b86c17aecaf9894e61b624f
shazaaly <shaza.aly@gmail.com>
2023-09-21 01:04:05 +0300

    delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,6 +43,7 @@ class FileStorage:
                 if obj == v:
                     key = k
             del FileStorage.__objects[key]
+            self.save()
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
