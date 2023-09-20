d1a4e395f96c783fcc36620b8670744969ccad72
shazaaly <shaza.aly@gmail.com>
2023-09-20 13:00:21 +0300

    remove state rel. from city class

--- models/city.py
+++ models/city.py
@@ -14,6 +14,5 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    state = relationship('State', back_populates='cities')
     places = relationship('Place', backref='cities',
                           cascade='all, delete, delete-orphan')
