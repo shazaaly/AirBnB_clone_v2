0b2060b3f9e9b3f74d38eab926ee81296e7e8e0a
shazaaly <shaza.aly@gmail.com>
2023-09-20 21:16:07 +0300

    storage type

--- models/amenity.py
+++ models/amenity.py
@@ -4,9 +4,14 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+import os
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    if os.getenv("HBNB_TYPE_STORAGE"):
+
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary=place_amenity)
+    else:
+        pass
