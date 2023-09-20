8e99792b4c0eb1be764c28c458261b110b2f52de
shazaaly <shaza.aly@gmail.com>
2023-09-20 13:10:55 +0300

    correct : add Base to review class

--- models/review.py
+++ models/review.py
@@ -4,25 +4,10 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """
     Definition of the Review class for the reviews table in the database.
-
-    Attributes:
-        __tablename__ (str): this attribute represents the table name,
-        "reviews".
-        text (str): this attribute represents a column containing a string
-        (1024 characters) and the attribute can't be null.
-        place_id (str): this attribute represents a column containing a
-        string (60 characters) and this attribute can't be null.
-        It is a foreign key to places.id.
-        user_id (str): this attribute represents a column containing a string
-        (60 characters) and it can't be null. Its a foreign key to users.id.
-
-    Args:
-        BaseModel (class): The BaseModel class.
-        Base (class): The declarative Base class from SQLAlchemy.
-    """
+   """
 
     __tablename__ = 'reviews'
 
