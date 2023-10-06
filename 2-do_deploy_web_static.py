c80488aec8476af8f717ae9d02e0fccdb0a53a6c
shazaaly <shaza.aly@gmail.com>
2023-10-07 01:02:41 +0300

    ب

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from datetime import datetime
-from fabric.api import env, put, run, local
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
 env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
         return False
@@ -17,14 +24,16 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
+
     if run("mkdir -p /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
+        return False
+
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
     if run("rm /tmp/{}".format(file)).failed is True:
         return False
