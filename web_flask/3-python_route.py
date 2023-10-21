4bb66fb053803b244050ad6caca59ecde95adce2
shazaaly <shaza.aly@gmail.com>
2023-10-21 10:28:13 +0300

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""ِ This The application listens on 0.0.0.0, port 5000.
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value\
+    of the text variable\
+        (replace underscore _ symbols with a space )
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """/hbnb route : display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def dynamic_route(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def dynamic_route_pyton(text="is cool"):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
