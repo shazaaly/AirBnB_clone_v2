f18ffb15524cf4ede515cab4573df759502ad8f6
shazaaly <shaza.aly@gmail.com>
2023-10-22 01:28:29 +0300

    f

--- main.py
+++ main.py
@@ -33,7 +33,5 @@ Verification
 print("")
 all_states = storage.all(State)
 for state_id, state in all_states.items():
-    if isinstance(state, State):
-        print("State: {}".format(state))
-        for city in state.cities:
-            print("Find the city {} in the state {}".format(city, state))
+    for city in state.cities:
+        print("Find the city {} in the state {}".format(city, state))
