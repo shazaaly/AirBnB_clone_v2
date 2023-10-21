a0e3e9ce01acc42309d044b2a2f366c6008b7d61
shazaaly <shaza.aly@gmail.com>
2023-10-21 10:07:25 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""ِ This The application listens on 0.0.0.0, port 5000.
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value\
+    of the text variable\
+        (replace underscore _ symbols with a space )
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """/hbnb route : display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<path:topic>', strict_slashes=False)
+def dynamic_route(topic):
+    return f"C {topic}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
