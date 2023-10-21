1518b3d3b8459774b3c04ac44b5abc910f1ce67c
shazaaly <shaza.aly@gmail.com>
2023-10-21 22:15:36 +0300

    f

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,7 +47,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def displayHTML(n):
+def number_template(n):
     """Display HTML only if n is an integer."""
     return render_template('5-number.html', n=n)
 
