defbf627a54f3841381cd3debeefb6f93882266c
shazaaly <shaza.aly@gmail.com>
2023-10-23 01:40:18 +0300

    ff

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 def states():
     """Displays cities per state"""
     states = storage.all(State)
-    return render_template("9-states.html", state=states)
+    return render_template("9-states.html", states=states)
 
 
 @app.route("/states/<id>", strict_slashes=False)
