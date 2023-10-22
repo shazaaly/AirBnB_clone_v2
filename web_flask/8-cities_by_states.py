b9ae0a0d9c21beac5ff336ff4ba283c035eb5a3f
shazaaly <shaza.aly@gmail.com>
2023-10-22 17:06:10 +0300

    fix route

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
 """
+A script to To load all cities of a State,
+If storage engine is DBStorage, use cities relationship
+Otherwise, use the public getter method cities.
+After each request remove the current SQLAlchemy Session,
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/cities_by_states: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+
+"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+
 
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def home():
+    """A function that serves as the home route of the application.
+    Returns:
+        str: The greeting message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    """Displays cities per state"""
+    states = storage.all(State)
+    return render_template("8-cities_by_states.py", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_context(ctx):
+    """Displays cities per state"""
     storage.close()
 
 
