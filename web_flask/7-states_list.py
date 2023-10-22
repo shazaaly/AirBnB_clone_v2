a4366b94fc110dfd8d6c4a752160036705407b95
shazaaly <shaza.aly@gmail.com>
2023-10-22 12:28:39 +0300

    fix

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+A script to To load all cities of a State,
+If storage engine is DBStorage, use cities relationship
+Otherwise, use the public getter method cities.
+After each request remove the current SQLAlchemy Session,
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/cities_by_states: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays cities per state"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_context(ctx):
+    """Displays cities per state"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
