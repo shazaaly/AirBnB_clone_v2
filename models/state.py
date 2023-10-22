d120ba67847993c3383e2bb362e5301cf121702c
shazaaly <shaza.aly@gmail.com>
2023-10-22 16:21:17 +0300

    check db type

--- models/state.py
+++ models/state.py
@@ -16,21 +16,22 @@ from models.base_model import Base
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          back_populates="state")
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all,delete", backref="state")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    else:
+        @property
+        def cities(self):
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                city = key.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.state_id == self.id):
+                    result.append(elem)
+            return result
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -31,7 +31,7 @@ def home():
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays cities per state"""
-    states = storage.all(State).values()
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
