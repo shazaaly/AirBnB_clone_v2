890d0621bcccbb5bc23fef7d5ac5aeedb7bca7d3
shazaaly <shaza.aly@gmail.com>
2023-10-23 11:01:45 +0300

    relation

--- models/amenity.py
+++ models/amenity.py
@@ -11,6 +11,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if os.getenv("HBNB_TYPE_STORAGE") == 'db':
         name = Column(String(128), nullable=False)
-        place_amenities = relationship('Place', secondary='place_amenity')
+        place_amenities = relationship(
+            'Place', secondary='place_amenity', back_populates="amenities")
     else:
         name = ""
--- models/place.py
+++ models/place.py
@@ -32,7 +32,7 @@ class Place(BaseModel, Base):
 
     __tablename__ = "places"
     if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        # city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
         user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
         description = Column(String(1024), nullable=True)
