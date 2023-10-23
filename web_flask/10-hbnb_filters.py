499cbb5d8d0c929e1ca8ce483b8ea30ac12b2ab5
shazaaly <shaza.aly@gmail.com>
2023-10-23 10:00:56 +0300

    10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,39 +1,25 @@
 #!/usr/bin/python3
 """
-A script to To load all cities of a State,
-If storage engine is DBStorage, use cities relationship
-Otherwise, use the public getter method cities.
-After each request remove the current SQLAlchemy Session,
-Declare a method to handle @app.teardown_appcontext
-Call in this method storage.close()
-Routes:
-/cities_by_states: display a HTML page: (inside the tag BODY)
-H1 tag: “States”
+route  :   /hbnb_filters: display a HTML page like 6-index.html,
+which was done during the project 0x01. AirBnB clone - Web static
 
 """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.amenity import Amenity
 
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays cities per state"""
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """display a HTML page like 6-index.html"""
     states = storage.all(State)
-    return render_template("9-states.html", states=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def state_id(id):
-    """Displays cities per state"""
-    states = storage.all(State).values()
-    for state in states:
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.py", states=states,
+                           amenities=amenities)
 
 
 app.debug = True
